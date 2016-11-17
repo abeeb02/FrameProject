387960aabd63bf7bb9f74f401154a55de2af587b
abeeb02 <ambernbeebe@yahoo.com>
2016-11-17 12:22:57 -0800

    Adding Frames
    attempting to add frames from group

--- FrameProject.java
+++ FrameProject.java
@@ -1,11 +1,73 @@
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-class FrameProject extends JFrame {
+class FrameOne implements ItemListener{
+	
+	FrameOne(){
+		JFrame frame = new JFrame("Frame One");
+		JRadioButton buttons[] = {new JRadioButton("Circles", false), new JRadioButton("Regions", false), new JRadioButton("Labels", false)};
+		frame.setSize(200,200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		for(JRadioButton button : buttons){
+			frame.add(button);
+			button.addItemListener(this);
+		}
+		frame.setLayout(new FlowLayout());
+		frame.setVisible(true);
+	}
+
+	public void itemStateChanged(ItemEvent e){
+		JRadioButton sel = (JRadioButton)e.getItemSelectable();
+		String text = sel.getText();
+		if(sel.isSelected()){
+			if(text.equals("Circles")){
+				// open frame 2
+			}else if(text.equals("Regions")){
+				// open frame 3
+			}else if(text.equals("Labels")){
+				FrameFour b = new FrameFour();
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		FrameOne a = new FrameOne();
+		new FrameTwo();
+	}
+}
+
+class FrameFour implements ActionListener{
+	private int count;
+	private Panel pane;
+	private String[] txt = {"Too expensive", "Too flimsy", "Too bulky", "Too feminine", "Too masculine"};
+	
+	FrameFour(){
+		JFrame frame = new JFrame("Frame Four");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 600);
+		pane = new Panel();
+		JButton button = new JButton("");
+		button.addActionListener(this);
+		frame.add(button, BorderLayout.CENTER);
+		frame.add(pane, BorderLayout.EAST);
+		frame.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(count < 5){
+				pane.add(new JLabel(txt[count]));
+				pane.revalidate();
+		}
+		count++;
+	}	
+}
+
+class FrameTwo extends JFrame {
    int x, y, h, w, x2, y2, r;
    double a, angle, count;
    
-   FrameProject() {   
+   FrameTwo() {   
       setSize(800, 800);
       setVisible(true);
       getContentPane().setBackground(Color.white);
@@ -51,11 +113,6 @@ class FrameProject extends JFrame {
          g.fillOval(x2, y2, r, r);
          a+=angle;
       }
-    
-    
-    }
-    public static void main(String args[]) {
-          new FrameProject();
     }
 }
 
